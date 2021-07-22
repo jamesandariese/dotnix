3f60d0b7b6c5c87c14f627c1f90d46224e1eaefb
James Andariese <james@strudelline.net>
2021-07-22 23:03:16 +0000

    wip

--- install.sh
+++ install.sh
@@ -40,6 +40,9 @@ stage1() {
     
     cd "$HOME"
     bash <(echo "$installer") --darwin-use-unencrypted-nix-store-volume --daemon
+    if [ -x /usr/bin/mdutil ];then
+    echo executing sudo /usr/bin/mdutil -i on /nix
+    sudo /usr/bin/mdutil -i on /nix
     stage2
 }
 
