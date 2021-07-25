a394468325e7c6b1601b72d340a220479920ba3a
James Andariese <james@strudelline.net>
2021-07-25 08:06:12 -0700

    wip

--- fetchGitNew.nix
+++ fetchGitNew.nix
@@ -1,13 +1,18 @@
 {pkgs ? import <nixpkgs> {}}:
 
-{
+rec {
+  buildGNUPGHOME = key:
+    pkgs.runCommand "gpg" {buildInputs = [pkgs.cacert];} ''
+      export GNUPGHOME="$out"
+      mkdir -p "$GNUPGHOME"
+      ${pkgs.gnupg}/bin/gpg --keyserver keys.openpgp.org --recv-key ${key}
+      rm $out/S.*
+    '';
   fetchGitGPG = repo: tag: key:
     pkgs.runCommand "git" {buildInputs = [pkgs.git pkgs.gnupg pkgs.cacert];} ''
-      export GNUPGHOME="$out/.gnupg"
+      export GNUPGHOME="${buildGNUPGHOME key}"
       ${pkgs.git}/bin/git clone "${repo}" "$out"
-      ${pkgs.gnupg}/bin/gpg --recv-key "${key}"
-      ${pkgs.git}/bin/git -C "$out" tag -v "${tag}"
       ${pkgs.git}/bin/git -C "$out" checkout "${tag}"
-      rm -rf "$GNUPGHOME"
+      ${pkgs.git}/bin/git -C "$out" verify-commit HEAD
     '';
 }
