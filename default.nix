68298944be34e2a109eceac6dbc233ff43b86171
James Andariese <james@strudelline.net>
2021-07-25 20:39:11 -0700

    change dotnix import to a callpackage

--- default.nix
+++ default.nix
@@ -2,6 +2,6 @@
 
 rec {
   inherit (pkgs.callPackage ./fetchGitNew.nix {}) fetchGitGPG;
-  dotnix = import (fetchGitGPG "https://github.com/jamesandariese/dotnix" "main" "DD4067DEE80475D7143D7F746BE4AD6FBF746F55");
+  dotnix = pkgs.callPackage (fetchGitGPG "https://github.com/jamesandariese/dotnix" "main" "DD4067DEE80475D7143D7F746BE4AD6FBF746F55") {};
   path = ./.;
 } 
