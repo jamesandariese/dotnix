4bccde24260fd1f3787f847509cfd2ec94842cbd
James Andariese <james@strudelline.net>
2021-07-25 20:17:11 -0700

    add root package verions

--- default.nix
+++ default.nix
@@ -2,5 +2,17 @@
 
 rec {
   path = ./.;
+  home-manager-path = builtins.fetchGit {
+      url = "https://github.com/nix-community/home-manager";
+      ref = "release-21.05";
+      rev = "9c0abed5228d54aad120b4bc757b6f5935aeda1c";
+    };
+  nixpkgs-path = builtins.fetchGit {
+      url = "https://github.com/nixos/nixpkgs";
+      ref = "nixos-unstable";
+      rev = "dd14e5d78e90a2ccd6007e569820de9b4861a6c2";
+    };
+  nixpkgs = pkgs.callPackage nixpkgs-path {};
+  home-manager = pkgs.callPackage home-manager-path {};
   home = pkgs.callPackage ./home.nix {};
 } 
